1da3c43efe05fd62460cdbc6bbf70f165a0b80e7
MamzelleA <amasetty@yahoo.fr>
2019-10-07 19:46:48 +0200

    Add View class to generate the view asked by user

---
+++ view/View.php
@@ -0,0 +1,37 @@
+<?php
+namespace OC_project_4\view;
+
+class View
+{
+	private $file;
+	private $template;
+	private $title;
+	private $subtitle;
+
+	public function __construct ($typeView, $action, $side)
+	{
+		$this->file = 'view/'.$typeView.'/'.$action.'_view.php';
+		$this->template = 'template/template_'.$side.'.php';
+	}
+
+	public function genView ($datas)
+	{
+		$content = $this->genFile($this->file, $datas);
+		$view = $this->genFile($this->template, array('title' => $this->title, 'subtitle' => $this->subtitle, 'content' => $content));
+		echo $view;
+	}
+
+	private function genFile($file, $datas)
+	{
+		if(file_exists($file))
+		{
+			extract($datas); //tableau associatif contenant éléments de $datas et les rendant accessibles
+			ob_start();
+			require $file;
+			return ob_get_clean();
+		}
+		else {
+			throw new Exception("Le fichier " .$file. " n'existe pas.");
+		}
+	}
+}
