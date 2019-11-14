9db30efb26993545d24af7e42275e304c7df916c
MamzelleA <amasetty@yahoo.fr>
2019-11-14 18:57:51 +0100

    fixing bug in Safari browser

--- view/frontend/chapters_view.php
+++ view/frontend/chapters_view.php
@@ -11,11 +11,11 @@
 	$extract = substr($extract,0,$lastSpace)." [...]";
 ?>
 	<div class="col-sm-12 border-bottom border-left border-1 border-light my-3">
-		<div class="button bg-transparent px-3" data-toggle="collapse" role="button" href="#num-<?= $chap['num_chap'] ?>" aria-expanded="false" aria-controls="author-resume" title="cliquez pour lire un extrait">
+		<a class="px-3" data-toggle="collapse" role="button" href="#num-<?= $chap['num_chap'] ?>" aria-expanded="false" aria-controls="author-resume" title="cliquez pour lire un extrait">
 			<div class="float-right mb-1"><?php if (isset($_COOKIE['numLastChap'])){if($chap['num_chap'] == $_COOKIE['numLastChap']){echo '<i class="fas fa-bookmark"></i>';}} ?></div>
 			<h4 class="pt-1">Chapitre <?php echo $chap['num_chap'] ?></h4>
 			<h3><?php echo $chap['title_chap'] ?></h3>
-		</div>
+		</a>
 		<div class="collapse" id="num-<?= $chap['num_chap'] ?>">
 			<p class="text-justify px-3"><?= $extract ?></p>
 			<div class="float-right p-3"><a title="lire le chapitre" href=<?="index.php?action=chapter&num=" .$chap['num_chap']?>><i class="fas fa-plus-circle"></i></a></div>
